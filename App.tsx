5a424a3225cc351092366032c8986c3efd3e244d
suyashpradhan <pradhansuyash39@gmail.com>
2024-11-22 20:38:16 +0530

    undo changes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import {StatusBar} from 'expo-status-bar';
 import {Platform, SafeAreaView, StyleSheet} from 'react-native';
-import {TodoApp} from "./components/TodoApp";
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
-            <TodoApp/>
             <StatusBar style="auto" translucent/>
         </SafeAreaView>
     );
